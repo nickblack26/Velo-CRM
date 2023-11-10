81f05299fe0cb24bb0047b9916eafc788da6a20c
Nick Black <nickblack@VELO-V201849.local>
2023-11-10 14:15:26 -0600

    Initial Commit

---
+++ Velo CRM/Velo_CRMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Velo_CRMApp.swift
+//  Velo CRM
+//
+//  Created by Nick Black on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Velo_CRMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
